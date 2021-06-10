d3d70bf0872138a59e3448434afc698f218f12dc
xieshu95 <xieshu95@gmail.com>
2021-06-10 13:59:16 +0200

    Function to create simulated data as observed data.

---
+++ R/DAISIE_sim.R
@@ -0,0 +1,34 @@
+#' Simulation fucntion to create simulated data as observed data in ABC.
+#'
+#' @param parameters A vector for CES rates.
+#' @param K Carrying capacity, Inf for diverdity-independent models.
+#' @param replicates The number of replicates(islands) for DAISIE simulation.
+#'
+#' @return A list with numeric vectors of diff statistics for:
+#' \itemize{
+#'   \item{\code{$spec_nltt_error}}
+#'   \item{\code{$num_spec_error}}
+#'   \item{\code{$num_col_error}}
+#'   \item{\code{$endemic_nltt_error}}
+#'   \item{\code{$nonendemic_nltt_error}}
+#' }
+#' @author Shu Xie
+#' @export
+
+
+DAISIE_sim <-  function(parameters, K, replicates){
+  sim <- list()
+  for (j in seq_len(replicates)) {
+    sim[[j]] <- DAISIE::DAISIE_sim_constant_rate(
+      time = 5,
+      M = 1000,
+      pars = c(parameters[1],parameters[2],K,parameters[3],parameters[4]),
+      replicates = 1,
+      sample_freq  = Inf,
+      plot_sims = FALSE,
+      verbose = FALSE,
+      cond = 0
+    )
+  }
+  return(sim)
+}
