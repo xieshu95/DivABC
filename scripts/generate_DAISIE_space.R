b0d242e54387d39e9bd230729bfba930efe4a60a
xieshu95 <xieshu95@gmail.com>
2021-06-29 16:34:21 +0200

    change seed to rep

--- scripts/generate_DAISIE_space.R
+++ scripts/generate_DAISIE_space.R
@@ -1,7 +1,7 @@
 ## create input parameter sets for ABC
 #1. ABC_DAISIE
 
-seed <- 1:10
+rep <- 1:10
 K <- c(40, Inf)
 lac <- c(0.2,0.4)
 mu <- c(0.1,0.2)
@@ -9,7 +9,7 @@ gam <- c(0.005,0.01)
 laa <-c(0.1,0.2)
 
 DAISIE_space <- expand.grid(
-  seed = seed,
+  rep = rep,
   lac = lac,
   mu = mu,
   gam = gam,
