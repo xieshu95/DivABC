057bbfb9340a8d4754d6f86326673e6570414150
xieshu95 <xieshu95@gmail.com>
2021-09-15 16:03:32 +0200

    default particles and iterations for ABC

--- R/run_ABC_DAISIE.R
+++ R/run_ABC_DAISIE.R
@@ -85,11 +85,11 @@ run_ABC_DAISIE <- function(param_space_name,
     init_epsilon_values = init_epsilon,
     prior_generating_function = prior_gen,
     prior_density_function = prior_dens,
-    number_of_particles = 20,
-    sigma = 0.5,
-    stop_rate = 0.002,
+    number_of_particles = 2000,
+    sigma = 0.2,
+    stop_rate = 0.001,
     replicates = 1,  ## simulation replicates for each parameter set
-    num_iterations = 4,
+    num_iterations = 10,
     K = as.numeric(obs_sim_pars$K),
     idparsopt = idparsopt
   )
