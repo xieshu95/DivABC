b20e090d19e33858e681b6cf08b9217b7d997b3e
xieshu95 <xieshu95@gmail.com>
2021-09-17 14:07:13 +0200

    set  as.numeric to idparsopt

--- scripts/run_MCMC_peregrine.R
+++ scripts/run_MCMC_peregrine.R
@@ -2,7 +2,7 @@ args <- commandArgs(TRUE)
 
 param_space_name <- args[1]
 param_set <- as.numeric(args[2])
-idparsopt <- args[3]
+idparsopt <- as.numeric(args[3])
 save_output <- TRUE
 metadata <- paste0("This is parameter set ", param_set)
 
@@ -11,6 +11,6 @@ library(TraisieABC)
 run_MCMC_DAISIE(
   param_space_name = args[1],
   param_set = as.numeric(args[2]),
-  idparsopt = args[3],
+  idparsopt = as.numeric(args[3]),
   save_output = save_output
 )
