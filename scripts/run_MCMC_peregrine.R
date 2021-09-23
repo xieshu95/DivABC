f5e37ba6dae98682e0b340cb1931406ffe9c1c9f
xieshu95 <xieshu95@gmail.com>
2021-09-23 22:25:52 +0200

    logical of each parameter

--- scripts/run_MCMC_peregrine.R
+++ scripts/run_MCMC_peregrine.R
@@ -2,7 +2,12 @@ args <- commandArgs(TRUE)
 
 param_space_name <- args[1]
 param_set <- as.numeric(args[2])
-idparsopt <- as.numeric(args[3])
+idparsopt_lac <- args[3]
+idparsopt_mu <- args[4]
+idparsopt_gam <- args[5]
+idparsopt_laa <- args[6]
+idparsopt_logical <- c(idparsopt_lac,idparsopt_mu,idparsopt_gam,idparsopt_laa)
+idparsopt <- which(idparsopt_logical == TRUE)
 save_output <- TRUE
 metadata <- paste0("This is parameter set ", param_set)
 
@@ -11,6 +16,6 @@ library(TraisieABC)
 run_MCMC_DAISIE(
   param_space_name = args[1],
   param_set = as.numeric(args[2]),
-  idparsopt = as.numeric(args[3]),
+  idparsopt = as.numeric(idparsopt),
   save_output = save_output
 )
