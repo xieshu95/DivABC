cd290e0c71cf1d19bc753df565ed45773cc77b83
xieshu95 <xieshu95@gmail.com>
2021-09-23 22:24:10 +0200

    add logical of each parameter in run_peregrine,to decide which parameter to optimize

--- bash/submit_run_ABC_peregrine.sh
+++ bash/submit_run_ABC_peregrine.sh
@@ -13,7 +13,10 @@ ml R
 Rscript -e "remotes::install_github('xieshu95/TraisieABC')"
 
 param_space_name=$1
-idparsopt=$2
+idparsopt_lac=$2
+idparsopt_mu=$3
+idparsopt_gam=$4
+idparsopt_laa=$5
 
 for_length=`wc -l TraisieABC/data/${param_space_name}.csv | cut -f1 -d' '`
 for_length=$(( ${for_length} - 1 ))
@@ -22,5 +25,8 @@ for (( param_set = 1; param_set <= $for_length; param_set++ ))
 do
 sbatch TraisieABC/bash/submit_run_ABC_param_set.sh ${param_space_name} \
                                                    ${param_set} \
-                                                   ${idparsopt}
+                                                   ${idparsopt_lac} \
+                                                   ${idparsopt_mu} \
+                                                   ${idparsopt_gam} \
+                                                   ${idparsopt_laa}
 done
--- bash/submit_run_MCMC_param_set.sh
+++ bash/submit_run_MCMC_param_set.sh
@@ -10,9 +10,15 @@
 # Arguments to follow the Rscript are as follows:
 param_space_name=$1
 param_set=$2
-idparsopt=$3
+idparsopt_lac=$3
+idparsopt_mu=$4
+idparsopt_gam=$5
+idparsopt_laa=$6
 
 ml R
 Rscript TraisieABC/scripts/run_MCMC_peregrine.R ${param_space_name} \
                                                ${param_set} \
-                                               ${idparsopt}
+                                               ${idparsopt_lac} \
+                                               ${idparsopt_mu} \
+                                               ${idparsopt_gam} \
+                                               ${idparsopt_laa}
