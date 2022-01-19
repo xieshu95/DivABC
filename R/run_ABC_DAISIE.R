c427d28ad7a113b0b56bc1e6ecd6d12dd020dd27
xieshu95 <xieshu95@gmail.com>
2022-01-19 04:47:54 +0100

    combine run_ABC_DAISIE AND run_ABC_traisie

--- R/run_ABC_DAISIE.R
+++ R/run_ABC_DAISIE.R
@@ -1,13 +1,19 @@
 #' Run ABC
-#'
+#' @param param_space_name A string with the parameter space to run. Can
+#'  be \code{"DAISIE_ABC"}, \code{"DAISIE_MCMC"},\code{"TraiSIE_ABC"}
+#' @param param_set A numeric of a specific line of parameter set in parameter space
+#' @param idparsopt A vector of positions of the parameters that be optimized.
+#' @param sim_model \code{"DAISIE"}, \code{"TraiSIE"}
+#' @param save_output A boolean to determine whether to save or return output.
 #' @author Shu Xie
 #' @return
 #' @export
 
-run_ABC_DAISIE <- function(param_space_name,
-                           param_set,
-                           idparsopt,
-                           save_output = TRUE){
+run_ABC <- function(param_space_name,
+                    param_set,
+                    idparsopt,
+                    sim_model = "DAISIE",
+                    save_output = TRUE){
 
   param_space <- load_param_space(param_space_name = param_space_name)
   seed <- param_set
@@ -23,25 +29,47 @@ run_ABC_DAISIE <- function(param_space_name,
   )
 
   obs_sim_pars <- param_space[param_set,]
-  obs_sim <- get_DAISIE_sim(parameters = c(obs_sim_pars$lac,
-                                           obs_sim_pars$mu,
-                                           obs_sim_pars$gam,
-                                           obs_sim_pars$laa),
-                            K = as.numeric(obs_sim_pars$K),
-                            replicates = 30)
+  if (sim_model == "DAISIE") {
+    obs_sim <- get_DAISIE_sim(parameters = c(obs_sim_pars$lac,
+                                             obs_sim_pars$mu,
+                                             obs_sim_pars$gam,
+                                             obs_sim_pars$laa),
+                              K = as.numeric(obs_sim_pars$K),
+                              replicates = 30)
+    sim_function <- get_DAISIE_sim
+    prior_generating_function <- prior_gen
+    prior_density_function <- prior_dens
+  } else if (sim_model == "TraiSIE") {
+    obs_sim <- get_TraiSIE_sim(parameters = as.numeric(c(obs_sim_pars$lac,
+                                                         obs_sim_pars$mu,
+                                                         obs_sim_pars$gam,
+                                                         obs_sim_pars$laa,
+                                                         obs_sim_pars$lac2,
+                                                         obs_sim_pars$mu2,
+                                                         obs_sim_pars$gam2,
+                                                         obs_sim_pars$laa2,
+                                                         obs_sim_pars$trans,
+                                                         obs_sim_pars$trans2)),
+                               K = as.numeric(obs_sim_pars$K),
+                               replicates = 30)
+    sim_function <- get_TraiSIE_sim
+    prior_generating_function <- prior_gen_trait
+    prior_density_function <- prior_dens_trait
+  }
+
   init_epsilon <- calc_epsilon_init(obs_sim)
 
   abc <- ABC_SMC_DAISIE (
     obs_data = obs_sim,
-    sim_function = get_DAISIE_sim,
+    sim_function = sim_function,
     init_epsilon_values = init_epsilon,
     prior_generating_function = prior_gen,
     prior_density_function = prior_dens,
-    number_of_particles = 2000,
+    number_of_particles = 5, #2000
     sigma = 1,
     stop_rate = 0.001,
     replicates = 1,  ## simulation replicates for each parameter set
-    num_iterations = 10,
+    num_iterations = 2,
     K = as.numeric(obs_sim_pars$K),
     idparsopt = as.numeric(idparsopt),
     fixpars = as.numeric(obs_sim_pars[2:5])
