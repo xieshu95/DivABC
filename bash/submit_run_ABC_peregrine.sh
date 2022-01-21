c6855a4c433b04ee2efccfa2ef2271f579d26850
xieshu95 <xieshu95@gmail.com>
2022-01-21 15:42:25 +0100

    install from master

--- bash/submit_run_ABC_peregrine.sh
+++ bash/submit_run_ABC_peregrine.sh
@@ -10,7 +10,7 @@
 
 # Start script
 ml R
-Rscript -e "remotes::install_github('xieshu95/TraisieABC@ss_test')"
+Rscript -e "remotes::install_github('xieshu95/TraisieABC')"
 
 param_space_name=$1
 idparsopt_lac=$2
