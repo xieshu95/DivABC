7f32fdc459692394c671b166a1853d7be4c898f3
xieshu95 <xieshu95@gmail.com>
2022-10-17 11:29:40 +0200

    create secsse space

--- scripts/generate_secsse_space.R
+++ scripts/generate_secsse_space.R
@@ -1,23 +1,18 @@
 ## create input parameter sets for scesse_ABC
 # two examined states and two concealed states
 ## 1. ETD_lam
-lam1 <- 0.2
-lam2 <- c(0.2,0.8)
-mu1 <- 0.1
-mu2 <- c(0.1,0.2)
-q12 <- 0.1
-q21 <- c(0.1,0.2)
-secsse <- expand.grid(
-  lam1 = lam1,
-  lam2 = lam2,
-  mu1 = mu1,
-  mu2 = mu2,
-  q12 = q12,
-  q21 = q21
-)
+par1 <- c(0.5,0.5,0,0,0.1,0.1)
+par2 <- c(0.3,0.7,0,0,0.1,0.1)
+par3 <- c(0.3,0.7,0,0,0.05,0.1)
+par4 <- c(0.3,0.7,0,0,0.1,0.05)
+par5 <- c(0.3,0.7,0.05,0.05,0.1,0.1)
+par6 <- c(0.3,0.7,0.05,0,0.1,0.1)
+secsse <- rbind(par1,par2,par3,par4,par5,par6)
+colnames(secsse) <- c("lam1","lam2","mu1","mu2","q12","q21")
+
 
 write.csv2(
   secsse,
-  "data/secsse.csv",
+  "data/secsse_ABC.csv",
   row.names = FALSE
 )
