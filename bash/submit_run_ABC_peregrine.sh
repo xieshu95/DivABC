48342bd89021a5cdb00f3b2fae1eab8a679b317b
xieshu95 <xieshu95@gmail.com>
2022-11-14 10:56:52 +0100

    larger accepted scale for secsse

--- bash/submit_run_ABC_peregrine.sh
+++ bash/submit_run_ABC_peregrine.sh
@@ -14,7 +14,7 @@
 
 # Start script
 ml R
-Rscript -e "remotes::install_github('xieshu95/TraisieABC@test-epsilon')"
+Rscript -e "remotes::install_github('xieshu95/TraisieABC@daisie')"
 
 param_space_name=${1}
 idparsopt_lac=${2}
