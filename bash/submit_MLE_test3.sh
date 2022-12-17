55bf232ede9c48931e4f0604437645677e05e102
xieshu95 <xieshu95@gmail.com>
2022-12-18 00:22:10 +0100

    MLE bash

---
+++ bash/submit_MLE_test3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --time=3-00:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=MLE
+#SBATCH --output=logs/MLE-%j.log
+#SBATCH --mem=1GB
+#SBATCH --partition=regular
+
+module load R
+Rscript TraisieABC/scripts/secsse/calc_MLE_cluster3.R
