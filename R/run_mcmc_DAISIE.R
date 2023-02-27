84737d9679f5cd1377f6dab1d8803ff4b466ad32
xieshu95 <xieshu95@gmail.com>
2023-02-28 00:32:43 +0100

    fix mcmc

--- R/run_mcmc_DAISIE.R
+++ R/run_mcmc_DAISIE.R
@@ -32,21 +32,21 @@ run_MCMC_DAISIE <- function(param_space_name,
 
   # obs_sim_pars_init <- obs_sim_pars + 0.0001
   initparsopt <- as.numeric(obs_sim_pars[c(1,2,3,4)]) + 0.00001
-  seed_mle <-as.integer(Sys.time()) %% 1000000L * sample(1:10,1)
-  set.seed(seed_mle)
+  seed_mcmc <-as.integer(Sys.time()) %% 1000000L * sample(1:10,1)
+  set.seed(seed_mcmc)
   message("seed_mle: ", seed_mle)
   for(n in 1:4){
     initparsopt[n]<-exp(log(initparsopt[n]) +
-                          stats::rnorm(1, 0, 1))
+                          stats::rnorm(1, 0, 0.01))
   }
   message("initial pars:", initparsopt)
   mcmc <- MCMC(datalist = obs_sim[[1]][[1]],
                log_lik_function = calc_log_lik_DAISIE,
                log_prior_function = calc_log_prior_DAISIE,
                parameters = as.numeric(initparsopt),
-               iterations = 1000000, ##1000000
-               burnin = 100000,   # 100000
-               thinning = 1000,
+               iterations = 500000, ##1000000
+               burnin = 10000,   # 100000
+               thinning = 500,
                sigma = 0.02,
                idparsopt = idparsopt)
 
