0b772602abc80ce566c90af007c58eb0182d8795
Shu Xie <xieshu95@gmail.com>
2023-04-04 22:40:09 +0200

    constrain at least 2 tips in each state

--- R/ABC_SMC.R
+++ R/ABC_SMC.R
@@ -99,7 +99,9 @@ ABC_SMC <- function( # nolint indeed a complex function
         if ("phy" %in% names(new_sim[[1]])) {
           if (length(new_sim[[1]]$examTraits) < 20 ||
               length(new_sim[[1]]$examTraits) >= 400 ||
-              length(unique(new_sim[[1]]$examTraits)) < 2) {
+              length(unique(new_sim[[1]]$examTraits)) < 2 ||
+              sum(new_sim[[1]]$examTraits == 1) < 2 ||
+              sum(new_sim[[1]]$examTraits == 2) < 2) {
             accept <- FALSE
           }
         }
