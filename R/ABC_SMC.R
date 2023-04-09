a59dcd7c9a9094f6d513735bceb937e253a698c2
Shu Xie <xieshu95@gmail.com>
2023-04-09 18:59:17 +0200

    delete check for secsse

--- R/ABC_SMC.R
+++ R/ABC_SMC.R
@@ -97,15 +97,15 @@ ABC_SMC <- function( # nolint indeed a complex function
 
         accept <- TRUE
         # for secsse
-        if ("phy" %in% names(new_sim[[1]])) {
-          if (length(new_sim[[1]]$examTraits) < 20 ||
-              length(new_sim[[1]]$examTraits) >= 400 ||
-              length(unique(new_sim[[1]]$examTraits)) < 2 ||
-              sum(new_sim[[1]]$examTraits == 1) < 2 ||
-              sum(new_sim[[1]]$examTraits == 2) < 2) {
-            accept <- FALSE
-          }
-        }
+        # if ("phy" %in% names(new_sim[[1]])) {
+        #   if (length(new_sim[[1]]$examTraits) < 20 ||
+        #       length(new_sim[[1]]$examTraits) >= 400 ||
+        #       length(unique(new_sim[[1]]$examTraits)) < 2 ||
+        #       sum(new_sim[[1]]$examTraits == 1) < 2 ||
+        #       sum(new_sim[[1]]$examTraits == 2) < 2) {
+        #     accept <- FALSE
+        #   }
+        # }
         #calculate the summary statistics for the simulated tree
         if (accept) {
           df_stats <- calc_ss_function (sim1 = obs_data[[1]],
