0c05b3b0478da82ca7012a886db16f7b79cecd60
Shu Xie <xieshu95@gmail.com>
2024-06-03 12:02:11 +0200

    update stop rate

--- R/ABC_SMC_secsse.R
+++ R/ABC_SMC_secsse.R
@@ -176,7 +176,7 @@ ABC_SMC_secsse <- function( # nolint indeed a complex function
                                  sum(init_state == "2A") + sum(init_state == "2B"))/length(init_state)
     ss_diff_list[[i]] <- ss_diff
     if (stoprate_reached == FALSE) {
-      epsilon[i + 1, ] <- apply(ss_diff, 2, quantile, probs = 0.5)
+      epsilon[i + 1, ] <- apply(ss_diff, 2, quantile, probs = 0.3)
     }
     ABC <- c()
     for (k in seq_along(new_params)) {
