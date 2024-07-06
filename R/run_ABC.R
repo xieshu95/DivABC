bdd9ad90b2a84cb1ba3304eba4b640efd5fa33b2
Shu Xie <xieshu95@gmail.com>
2024-07-06 04:32:41 +0200

    simplify

--- R/run_ABC.R
+++ R/run_ABC.R
@@ -35,13 +35,6 @@ run_ABC <- function(param_space_name,
   obs_sim <- load_obs_sim(param_space_name = param_space_name)[[param_set]]
 
   if (sim_model == "DAISIE") {
-    sim_function <- get_DAISIE_sim
-    calc_ss_function <- calc_ss_diff_daisie
-    prior_generating_function <- prior_gen
-    prior_density_function <- prior_dens
-    fixpars = as.numeric(obs_sim_pars[1:4])
-
-    # init_epsilon_all <- c(150,150,50,50,10,10)
     if(ss_set == 0){ # all
       init_epsilon <- c(150,50,50,20,20,150,50,50)
     } else if (ss_set == 1){  # phylogenetic-- nltt+sd
@@ -54,28 +47,21 @@ run_ABC <- function(param_space_name,
 
     abc <- ABC_SMC (
       obs_data = obs_sim,
-      sim_function = sim_function,
-      calc_ss_function = calc_ss_function,
+      sim_function <- get_DAISIE_sim,
+      calc_ss_function <- calc_ss_diff_daisie,
       init_epsilon_values = init_epsilon,
-      prior_generating_function = prior_generating_function,
-      prior_density_function = prior_density_function,
+      prior_generating_function <- prior_gen,
+      prior_density_function <- prior_dens,
       number_of_particles = 500,
       sigma = 0.2,
       stop_rate = 0.001,
-      replicates = 1,
-      num_iterations = 15,
-      K = as.numeric(obs_sim_pars$K),
+      num_iterations = 20,
       idparsopt = as.numeric(idparsopt),
-      fixpars = fixpars,
+      pars = as.numeric(obs_sim_pars[1:4]),
       ss_set = ss_set
     )
 
   } else if (sim_model == "TraiSIE") {
-    sim_function <- get_TraiSIE_sim
-    calc_ss_function <- calc_ss_diff_traisie
-    prior_generating_function <- prior_gen_trait
-    prior_density_function <- prior_dens_trait
-    fixpars = as.numeric(obs_sim_pars[c(1:10)])
 
     init_epsilon_all <- c(100,20,5,5,10,10,10)
     if(ss_set == 0){
@@ -85,30 +71,21 @@ run_ABC <- function(param_space_name,
     }
     abc <- ABC_SMC (
       obs_data = obs_sim,
-      sim_function = sim_function,
-      calc_ss_function = calc_ss_function,
+      sim_function <- get_TraiSIE_sim,
+      calc_ss_function <- calc_ss_diff_traisie,
+      prior_generating_function <- prior_gen_trait,
+      prior_density_function <- prior_dens_trait,
       init_epsilon_values = init_epsilon,
-      prior_generating_function = prior_generating_function,
-      prior_density_function = prior_density_function,
       number_of_particles = 500,
       sigma = 0.1,
       stop_rate = 0.002,
-      replicates = 1,
       num_iterations = 15,
-      K = as.numeric(obs_sim_pars$K),
       idparsopt = as.numeric(idparsopt),
-      fixpars = fixpars,
+      fixpars = as.numeric(obs_sim_pars[c(1:10)]),
       ss_set = ss_set
     )
 
   } else if (sim_model == "secsse") {
-    sim_function <- get_secsse_sim
-    calc_ss_function <- calc_ss_diff_secsse
-    prior_generating_function <- prior_gen_secsse
-    prior_density_function <- prior_dens_secsse
-    fixpars = as.numeric(obs_sim_pars[1:6])
-    # init_epsilon <- calc_epsilon_init_secsse(sim = obs_sim)
-    # init_epsilon_all <- c(50,50,10,10,100,100,1,1,1)
     if(ss_set == 0){ # nltt + nltt1 + nltt2 + D
       init_epsilon <- c(1,1,1,1)
     } else if (ss_set == 1){ # nltt + nltt1 + nltt2
@@ -128,19 +105,17 @@ run_ABC <- function(param_space_name,
     }
     abc <- ABC_SMC_secsse (
       obs_data = obs_sim,
-      sim_function = sim_function,
-      calc_ss_function = calc_ss_function,
+      sim_function <- get_secsse_sim,
+      calc_ss_function <- calc_ss_diff_secsse,
+      prior_generating_function <- prior_gen_secsse,
+      prior_density_function <- prior_dens_secsse,
       init_epsilon_values = init_epsilon,
-      prior_generating_function = prior_generating_function,
-      prior_density_function = prior_density_function,
       number_of_particles = 500,
       sigma = 0.2,
       stop_rate = 0.001,
-      replicates = 1,
-      num_iterations = 15,
-      K = as.numeric(obs_sim_pars$K),
+      num_iterations = 20,
       idparsopt = as.numeric(idparsopt),
-      fixpars = fixpars,
+      fixpars = as.numeric(obs_sim_pars[1:6]),
       ss_set = ss_set
     )
   }
