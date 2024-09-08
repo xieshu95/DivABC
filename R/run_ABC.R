4633ae9191677eb26a02b4cd99220d5c543a0614
Shu Xie <xieshu95@gmail.com>
2024-09-08 09:59:27 +0200

    more iterations

--- R/run_ABC.R
+++ R/run_ABC.R
@@ -113,7 +113,7 @@ run_ABC <- function(param_space_name,
       number_of_particles = 500,
       sigma = 0.2,
       stop_rate = 0.0005,
-      num_iterations = 20,
+      num_iterations = 30,
       idparsopt = as.numeric(idparsopt),
       fixpars = as.numeric(obs_sim_pars[1:6]),
       ss_set = ss_set
