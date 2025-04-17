7cbcdb13f15414b8b8b27e5301ec2c8fdf2145ee
Shu Xie <xieshu95@gmail.com>
2025-04-17 13:41:16 +0800

    run cluster

--- bash/submit_run_bisse_ABC_param_set.sh
+++ bash/submit_run_bisse_ABC_param_set.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --time=9-23:00:00
+#SBATCH --time=6-23:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --job-name=test
+#SBATCH --job-name=scen2
 #SBATCH --output=logs/ABC-%j.log
 #SBATCH --mem=1GB
 #SBATCH --partition=gelifes
