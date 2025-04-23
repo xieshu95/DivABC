3e2a553a6fb65ff84c677d1da5bb904cafac1282
Shu Xie <xieshu95@gmail.com>
2025-04-23 11:06:41 +0800

    musse MCMC test

--- R/run_mcmc_musse.R
+++ R/run_mcmc_musse.R
@@ -38,9 +38,9 @@ run_MCMC_musse <- function(param_space_name,
                log_lik_function = calc_log_lik_musse,
                log_prior_function = calc_log_prior_musse,
                parameters = as.numeric(initparsopt),
-               iterations = 1000000,
-               burnin = 10000,
-               thinning = 100,
+               iterations = 10000,
+               burnin = 1,
+               thinning = 1,
                sigma = 0.01,
                idparsopt = idparsopt)
 
