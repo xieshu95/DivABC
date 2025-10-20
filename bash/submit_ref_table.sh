a111b597d46c8fcb5142cd3a12e8a3521f1a63d7
Shu Xie <xieshu95@gmail.com>
2025-10-20 14:12:09 +0800

    fix bash

--- bash/submit_ref_table.sh
+++ bash/submit_ref_table.sh
@@ -2,8 +2,8 @@
 #SBATCH --time=1:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --job-name=start_ABC
-#SBATCH --output=logs/start_ABC.log
+#SBATCH --job-name=start_ABCrf
+#SBATCH --output=logs/start_ABCrf.log
 #SBATCH --mem=1GB
 #SBATCH --partition=regular
 
@@ -15,5 +15,5 @@ Rscript -e "remotes::install_github('xieshu95/DivABC@abcrf')"
 
 for (( id = 1; id <= 3; id++ ))
   do
-Rscript DivABC/scripts/run_ABC_bisse_peregrine.R ${id}
+Rscript DivABC/R/create_ref_table.R ${id}
 done
