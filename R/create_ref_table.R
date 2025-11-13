aa30563eed8479166207afb759c0c415a6dff2c5
Shu Xie <xieshu95@gmail.com>
2025-11-13 12:25:54 +0800

    more reps run on cluster

--- R/create_ref_table.R
+++ R/create_ref_table.R
@@ -15,7 +15,7 @@ create_ref_table <- function(
   library(R.utils)
   library(MASS)
   set.seed(100)
-  n_sim   <- 3
+  n_sim   <- 2000
   message("id: ", id)
   # generate training data (reference table)
   tmp <- DivABC:::get_bisse_sim_create_obs(parameters = c(0.2,0.1,0.1,0.1,0.1,0.1),
