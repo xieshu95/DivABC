d8bc899ebde3ef694f34fd5c67645f1933e2e77f
Shu Xie <xieshu95@gmail.com>
2025-12-25 16:58:34 +0800

    fix tests

--- tests/testthat/test-ABC_SMC.R
+++ tests/testthat/test-ABC_SMC.R
@@ -11,35 +11,36 @@ test_that("test ABC_SMC output is correct", {
   obs_sim_pars <- param_space[1,]
   obs_sim <- load_obs_sim(param_space_name = "DAISIE_ABC_DI")[[1]]
   ss_set = 3
-  init_epsilon <- c(100,20,20)
+  init_epsilon <- c(500,100,100)
   idparsopt = 1:4
   ABC <- ABC_SMC (
     obs_data = obs_sim,
-    sim_function <- get_DAISIE_sim,
+    sim_function <- get_DAISIE_sim_DI,
     calc_ss_function <- calc_ss_diff_daisie,
     init_epsilon_values = init_epsilon,
-    prior_generating_function <- prior_gen,
-    prior_density_function <- prior_dens,
+    prior_generating_function <- prior_gen_DI,
+    prior_density_function <- prior_dens_DI,
     number_of_particles = 5,
     sigma = 0.2,
     stop_rate = 0.01,
-    num_iterations = 3,
+    num_iterations = 2,
     idparsopt = as.numeric(idparsopt),
     pars = as.numeric(obs_sim_pars[1:4]),
     ss_set = 3
   )
   expect_length(ABC, 6)
   expect_length(ABC$sim_list, 5)
-  expect_length(ABC$ABC, 3)
-  expect_equal(ABC$n_iter, 3)
-  expect_length(ABC$ss_diff_list, 3)
+  expect_length(ABC$ABC, 2)
+  expect_equal(ABC$n_iter, 2)
+  expect_length(ABC$ss_diff_list, 2)
 })
 
 
-test_that("test ABC_SMC_secsse output is correct", {
+test_that("test ABC_SMC_bisse output is correct", {
 
   param_space <- load_param_space(
-    param_space_name = "secsse_ABC_test")
+    param_space_name = "bisse_ABC_test")
+  param_space_name = "bisse_ABC_test"
   set.seed(
     1,
     kind = "Mersenne-Twister",
@@ -47,28 +48,29 @@ test_that("test ABC_SMC_secsse output is correct", {
     sample.kind = "Rejection"
   )
   obs_sim_pars <- param_space[1,]
-  obs_sim <- load_obs_sim(param_space_name = "secsse_ABC_test")[[1]]
+  obs_sim <- load_obs_sim(param_space_name = "bisse_ABC_test")[[1]]
   ss_set = 1
   init_epsilon <- c(1,1,1)
   idparsopt = 1:6
-  ABC <- ABC_SMC_secsse (
+  ABC <- ABC_SMC_bisse (
     obs_data = obs_sim,
     sim_function <- get_bisse_sim,
-    calc_ss_function <- calc_ss_diff_secsse,
-    prior_generating_function <- prior_gen_secsse,
-    prior_density_function <- prior_dens_secsse,
+    calc_ss_function <- calc_ss_diff_bisse,
+    prior_generating_function <- prior_gen_bisse,
+    prior_density_function <- prior_dens_bisse,
     init_epsilon_values = init_epsilon,
     number_of_particles = 5,
     sigma = 0.2,
     stop_rate = 0.01,
-    num_iterations = 3,
+    num_iterations = 2,
     idparsopt = as.numeric(idparsopt),
-    fixpars = as.numeric(obs_sim_pars[1:6]),
+    pars = as.numeric(obs_sim_pars[1:6]),
     ss_set = ss_set
   )
-  expect_length(ABC, 8)
+
+  expect_length(ABC, 6)
   expect_length(ABC$sim_list, 5)
-  expect_length(ABC$ABC, 3)
-  expect_equal(ABC$n_iter, 3)
-  expect_length(ABC$ss_diff_list, 3)
+  expect_length(ABC$ABC, 2)
+  expect_equal(ABC$n_iter, 2)
+  expect_length(ABC$ss_diff_list, 2)
 })
--- tests/testthat/test-calc_loglik_prior.R
+++ tests/testthat/test-calc_loglik_prior.R
@@ -2,19 +2,19 @@ test_that("test calc_loglik_prior output is correct with DAISIE model", {
 
   datalist <- load_obs_sim(param_space_name = "DAISIE_ABC_DI")[[1]][[1]][[1]]
   params <- c(0.4,0.1,0.003,0.1)
-  loglike <- calc_log_lik_DAISIE(params = params, datalist = datalist)
+  loglik <- calc_log_lik_DAISIE_DI(params = params, datalist = datalist)
 
-  expect_length(loglike, 1)
-  expect_equal(loglike, -111.6417957812863)
+  expect_length(loglik, 1)
+  expect_equal(loglik, -111.641793456707)
 })
 
 
 test_that("test calc_loglik_prior output is correct with SecSSE model", {
 
-  datalist <- load_obs_sim(param_space_name = "secsse_ABC_test")[[1]][[1]]
+  datalist <- load_obs_sim(param_space_name = "bisse_ABC_test")[[1]][[1]]
   params <- c(0.3,0.3,0.05,0.05,0.05,0.05)
-  loglike <- calc_log_lik_secsse(params = params, datalist = datalist)
+  loglik <- calc_log_lik_bisse(params = params, datalist = datalist)
 
-  expect_length(loglike, 1)
-  expect_equal(loglike, -141.5576266553215)
+  expect_length(loglik, 1)
+  expect_equal(loglik, -141.5576266553215)
 })
